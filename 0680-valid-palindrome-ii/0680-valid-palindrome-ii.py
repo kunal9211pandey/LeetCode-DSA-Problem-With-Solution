401b120bc9bf7c69492c534d85d4414f27c3ba9b
Kunal Pandey <118272078+kunal9211pandey@users.noreply.github.com>
2026-01-15 19:53:11 +0530

    Time: 55 ms (75.66%), Space: 19.7 MB (11.89%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,5 +1,5 @@
 class Solution:
-    def is_palindrome(self , s , left ,right):
+    def is_palindrome(self, s, left, right):
         while left < right:
             if s[left] != s[right]:
                 return False
@@ -8,14 +8,16 @@ class Solution:
         return True
 
     def validPalindrome(self, s: str) -> bool:
-        left = 0
-        right = len(s) - 1
+        i, j = 0, len(s) - 1
 
-        while left < right:
-            if s[left] != s[right]:
-                return self.is_palindrome(s , left+1 ,right) or self.is_palindrome(s , left ,right-1)
-            left += 1
-            right -= 1
+        while i < j:
+            if s[i] == s[j]:
+                i += 1
+                j -= 1
+            else:
+                return (
+                    self.is_palindrome(s, i + 1, j) or
+                    self.is_palindrome(s, i, j - 1)
+                )
+
         return True
-         
-        
