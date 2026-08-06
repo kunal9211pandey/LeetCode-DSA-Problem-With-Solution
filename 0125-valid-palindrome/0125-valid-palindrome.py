59d9eef16ae965eced1ac3a26faa42d3cf614f40
Kunal Pandey <118272078+kunal9211pandey@users.noreply.github.com>
2026-08-06 20:02:58 +0530

    Time: 3 ms (99.02%), Space: 20.3 MB (25.96%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,15 +1,22 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        i =0
-        j =len(s) - 1
+        # Convert all upper to lower case
+        s = s.lower()
 
+        # keep only alphanumeric char
+        s = "".join(ch for ch in s if ch.isalnum())
+
+        i = 0
+        j = len(s)-1
+        
         while i < j:
-            while i < j and not s[i].isalnum():
-                i += 1
-            while i < j and not s[j].isalnum():
-                j -= 1
-            if s[i].lower() != s[j].lower():
+            if s[i] != s[j]:
                 return False
             i += 1
             j -= 1
         return True
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
